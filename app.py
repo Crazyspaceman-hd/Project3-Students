ad222ba5ae2bf1015d8ca51d639bddeb34b271d4
unknown <hvdeane@gmail.com>
2021-06-05 09:43:44 -0700

    deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, jsonify, request
-from flask_cors import CORS
+
 from flask_pymongo import PyMongo
 import pymongo
 
@@ -11,7 +11,7 @@ import json
 
 
 app = Flask(__name__)
-cors= CORS(app)
+
 
 app.config["DEBUG"] = True
 
@@ -21,16 +21,9 @@ mongo=PyMongo(app)
 math=mongo.db.math
 portuguese=mongo.db.portuguese
 
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 @app.route("/")
-@cross_origin()
-def helloWorld():
-  return "Hello, cross-origin-world!"
-
-# @app.route("/")
-# def main():
-#     return render_template("index.html")
+def main():
+    return render_template("index.html")
 
 @app.route("/math")
 def stu_math():
